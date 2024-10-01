06c1cc7bbbea2848ea69088c37983a268edf23dd
sudoaugustin <sudoaugustin@gmail.com>
2024-10-01 20:08:50 +0630

    comment: unused files in docs

--- apps/docs/codes/comparisions/skel-ui/page.tsx
+++ apps/docs/codes/comparisions/skel-ui/page.tsx
@@ -1,24 +1,24 @@
-"use client";
-import Skel from "@skel-ui/react";
-import Image from "next/image";
-import "@skel-ui/react/styles.css";
-import useProfile from "../useProfile";
+// "use client";
+// import Skel from "@skel-ui/react";
+// import Image from "next/image";
+// import "@skel-ui/react/styles.css";
+// import useProfile from "../useProfile";
 
-export default function Page() {
-  const { user, isLoading } = useProfile();
+// export default function Page() {
+//   const { user, isLoading } = useProfile();
 
-  return (
-    <Skel.Root
-      isLoading
-      className="flex space-x-4 text-black bg-white border-slate-50 w-full max-w-sm p-2.5 rounded-lg"
-    >
-      <Skel.Item as="div" className="size-20 rounded-full overflow-hidden relative loading:rounded-full">
-        <Image src={user?.avatar as string} alt="" fill className="object-cover" />
-      </Skel.Item>
-      <div className="flex-1 space-y-1.5">
-        <Skel.Item className="text-xl font-bold loading:w-1/2">{user?.name}</Skel.Item>
-        <Skel.Item className="text-sm font-medium loading:h-10">{user?.bio}</Skel.Item>
-      </div>
-    </Skel.Root>
-  );
-}
+//   return (
+//     <Skel.Root
+//       isLoading
+//       className="flex space-x-4 text-black bg-white border-slate-50 w-full max-w-sm p-2.5 rounded-lg"
+//     >
+//       <Skel.Item as="div" className="size-20 rounded-full overflow-hidden relative loading:rounded-full">
+//         <Image src={user?.avatar as string} alt="" fill className="object-cover" />
+//       </Skel.Item>
+//       <div className="flex-1 space-y-1.5">
+//         <Skel.Item className="text-xl font-bold loading:w-1/2">{user?.name}</Skel.Item>
+//         <Skel.Item className="text-sm font-medium loading:h-10">{user?.bio}</Skel.Item>
+//       </div>
+//     </Skel.Root>
+//   );
+// }
--- apps/docs/codes/comparisions/useProfile.ts
+++ apps/docs/codes/comparisions/useProfile.ts
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-type User = {
-  age: number;
-  avatar: string;
-};
+// type User = {
+//   age: number;
+//   avatar: string;
+// };
 
-export default function useProfile() {
-  const [user, setUser] = useState<User>();
+// export default function useProfile() {
+//   const [user, setUser] = useState<User>();
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setUser({
-        age: 20,
-        avatar:
-          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=512&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      });
-    }, 5000);
+//   useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       setUser({
+//         age: 20,
+//         avatar:
+//           "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=512&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//       });
+//     }, 5000);
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
+//     return () => {
+//       clearTimeout(timeout);
+//     };
+//   }, []);
 
-  return { user, isLoading: !user };
-}
+//   return { user, isLoading: !user };
+// }
